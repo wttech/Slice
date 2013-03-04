6b9d826187bb9a220c49b89a7f9aefe70e10dd9d
kamilciecierski <kamil.ciecierski@MILLION.cognet.local>
2013-03-04 10:37:23 +0100

    Added informations about null returning by ModelProvider class methods.

--- slice-core-api/src/main/java/com/cognifide/slice/api/provider/ModelProvider.java
+++ slice-core-api/src/main/java/com/cognifide/slice/api/provider/ModelProvider.java
@@ -1,27 +1,16 @@
 package com.cognifide.slice.api.provider;
 
 /*
- * #%L
- * Slice - Core API
- * $Id:$
- * $HeadURL:$
- * %%
- * Copyright (C) 2012 Cognifide Limited
- * %%
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * #%L Slice - Core API $Id:$ $HeadURL:$ %% Copyright (C) 2012 Cognifide Limited %% Licensed under the Apache
+ * License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You
+ * may obtain a copy of the License at
  * 
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * #L%
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the License. #L%
  */
-
 
 import java.util.Iterator;
 import java.util.List;
@@ -41,8 +30,8 @@ public interface ModelProvider {
 	 * {@link ExecutionContextStack} is modified - path attribute is added on top of execution stack. It
 	 * allows for recursive call of ModelProvider methods from model object that is being created.
 	 * 
-	 * It is possible to use absolute and relative (with "./" prefix) paths in recursive calls.
-	 * All gets are performed with Context that was used to create this ModelProvider.
+	 * It is possible to use absolute and relative (with "./" prefix) paths in recursive calls. All gets are
+	 * performed with Context that was used to create this ModelProvider.
 	 * 
 	 * <code>
 	 *   modelProvider.get(ModelType.class, "/absolute/patch");
@@ -54,7 +43,9 @@ public interface ModelProvider {
 	 * @parem T type of model object to create
 	 * @param type class of model object to create
 	 * @param path CRX repository path to create object from
-	 * @return model object from given CRX repository path
+	 * @return model object from given CRX repository path.Cannot return code>null</code>.
+	 * @throws ConfigurationException - if this injector cannot find or create the provider.
+	 * @throws ProvisionException - if there was a runtime failure while providing an instance.
 	 */
 	<T> T get(final Class<T> type, final String path);
 
@@ -68,7 +59,9 @@ public interface ModelProvider {
 	 * @parem T type of model object to create
 	 * @param type class of model object to create
 	 * @param resource Sling resource to create object from
-	 * @return model object from given resource
+	 * @return model object from given resource. Cannot return code>null</code>.
+	 * @throws ConfigurationException - if this injector cannot find or create the provider.
+	 * @throws ProvisionException - if there was a runtime failure while providing an instance.
 	 */
 	<T> T get(final Class<T> type, final Resource resource);
 
@@ -79,24 +72,27 @@ public interface ModelProvider {
 	 * 
 	 * @param className canonical name of a class to be resolved, e.g. com.cognifide.slice.api.ModelProvider
 	 * @param path CRX repository path to create object from
-	 * @return model object of specified className from given CRX repository path
-	 * @throws ClassNotFoundException if specified className cannot be resolved by Injector,
+	 * @return model object of specified className from given CRX repository path. Cannot return
+	 * code>null</code>.
+	 * @throws ClassNotFoundException if specified className cannot be resolved by Injector
+	 * @throws ConfigurationException - if this injector cannot find or create the provider.
+	 * @throws ProvisionException - if there was a runtime failure while providing an instance.
 	 * {@link ClassNotFoundException} is thrown
 	 */
 	Object get(final String className, final String path) throws ClassNotFoundException;
 
 	/**
 	 * Creates list of model objects of type T from given CRX repository paths. During this method call state
-	 * of {@link ExecutionContextStack} is modified for each path from iterator - path is added on top of
-	 * path stack. It allows for recursive call of ModelProvider methods from model objects that are being
-	 * created.
+	 * of {@link ExecutionContextStack} is modified for each path from iterator - path is added on top of path
+	 * stack. It allows for recursive call of ModelProvider methods from model objects that are being created.
 	 * 
 	 * It is possible to use absolute and relative (with "./" prefix) paths in recursive calls.
 	 * 
 	 * @param T type of model objects to create
 	 * @param type class of model objects to create
 	 * @param paths iterator that returns CRX repository paths to create objects from
-	 * @return list of model objects from given CRX repository paths
+	 * @return list of model objects from given CRX repository paths .Returns empty list if <i>paths</i>
+	 * argument is <code>null</code>.
 	 */
 	<T> List<T> getList(final Class<T> type, final Iterator<String> paths);
 
@@ -106,7 +102,8 @@ public interface ModelProvider {
 	 * @param T type of model objects to create
 	 * @param type class of model objects to create
 	 * @param paths array of CRX repository paths to create objects from
-	 * @return list of model objects from given CRX repository paths
+	 * @return list of model objects from given CRX repository paths. Returns empty list if <i>paths</i>
+	 * argument is <code>null</code>.
 	 */
 	<T> List<T> getList(final Class<T> type, final String[] paths);
 }
