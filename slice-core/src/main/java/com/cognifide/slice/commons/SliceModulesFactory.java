52c6769387339f45537d897052c66badf7cd20c4
Tomasz Rękawek <trekawek@gmail.com>
2013-10-29 20:05:37 +0100

    removed unused ContextProviderFactory & SimpleContextProvider

--- slice-core/src/main/java/com/cognifide/slice/commons/SliceModulesFactory.java
+++ slice-core/src/main/java/com/cognifide/slice/commons/SliceModulesFactory.java
@@ -8,7 +8,6 @@ import org.osgi.framework.BundleContext;
 
 import com.cognifide.slice.api.context.ContextScope;
 import com.cognifide.slice.core.internal.context.SliceContextScope;
-import com.cognifide.slice.core.internal.module.ContextModule;
 import com.cognifide.slice.core.internal.module.JcrModule;
 import com.cognifide.slice.core.internal.module.LinkModule;
 import com.cognifide.slice.core.internal.module.SliceModule;
@@ -63,7 +62,6 @@ public class SliceModulesFactory {
 		modules.add(new SlingModule(contextScope));
 		modules.add(new JcrModule());
 		modules.add(new LinkModule());
-		modules.add(new ContextModule());
 		return modules;
 	}
 }
