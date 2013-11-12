eb38ac9fe28552e32b3f519a75d7dc1fad69f608
Krzysztof Ryk <krzysiek.ryk@gmail.com>
2013-11-12 16:09:30 +0100

    Code formatting

--- slice-core-api/src/main/java/com/cognifide/slice/api/tag/SliceLookupTei.java
+++ slice-core-api/src/main/java/com/cognifide/slice/api/tag/SliceLookupTei.java
@@ -5,31 +5,32 @@ import javax.servlet.jsp.tagext.TagExtraInfo;
 import javax.servlet.jsp.tagext.VariableInfo;
 
 /**
- * User: krzysztof.ryk@solsoft.pl
- * Date: 11/7/13 1:07 PM
+ * User: krzysztof.ryk@solsoft.pl Date: 11/7/13 1:07 PM
  */
 public class SliceLookupTei extends TagExtraInfo {
 
-   private static final String ATTRIBUTE_CLS = "cls";
-   private static final String ATTRIBUTE_VAR = "var";
-   private static final String DEFAULT_TYPE = "java.lang.Object";
+	private static final String ATTRIBUTE_CLS = "cls";
 
-   /**
-    * Get variable info for controller tag. This method adds additional info to object placed in the page context.
-    * In general, this additional info references to model class. This approach allows us to use Code Completion feature
-    * in our IDE's (works in Idea IntelliJ, not tested on Eclipse)
-    *
-    * @param data Tag data
-    * @return Variable infos - info containing model class
-    */
-   @Override
-   public VariableInfo[] getVariableInfo(TagData data) {
-      String cls = data.getAttributeString(ATTRIBUTE_CLS);
-      if (cls == null) {
-         cls = DEFAULT_TYPE;
-      }
-      return new VariableInfo[]{
-            new VariableInfo(data.getAttributeString(ATTRIBUTE_VAR), cls, true, VariableInfo.AT_BEGIN)};
-   }
+	private static final String ATTRIBUTE_VAR = "var";
+
+	private static final String DEFAULT_TYPE = "java.lang.Object";
+
+	/**
+	 * Get variable info for controller tag. This method adds additional info to object placed in the page
+	 * context. In general, this additional info references to model class. This approach allows us to use
+	 * Code Completion feature in our IDE's (works in Idea IntelliJ, not tested on Eclipse)
+	 * 
+	 * @param data Tag data
+	 * @return Variable infos - info containing model class
+	 */
+	@Override
+	public VariableInfo[] getVariableInfo(TagData data) {
+		String cls = data.getAttributeString(ATTRIBUTE_CLS);
+		if (cls == null) {
+			cls = DEFAULT_TYPE;
+		}
+		return new VariableInfo[] { new VariableInfo(data.getAttributeString(ATTRIBUTE_VAR), cls, true,
+				VariableInfo.AT_BEGIN) };
+	}
 
 }
