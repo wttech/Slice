cfe596e95211d20017f5c976be4bdca907f95e09
Tomasz Rękawek <trekawek@gmail.com>
2013-11-13 22:30:04 +0100

    added javadoc & license

--- slice-core/src/main/java/com/cognifide/slice/commons/provider/KeyCache.java
+++ slice-core/src/main/java/com/cognifide/slice/commons/provider/KeyCache.java
@@ -1,3 +1,25 @@
+/*-
+ * #%L
+ * Slice - Core
+ * $Id:$
+ * $HeadURL:$
+ * %%
+ * Copyright (C) 2012 Cognifide Limited
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 package com.cognifide.slice.commons.provider;
 
 import java.util.HashMap;
@@ -15,6 +37,13 @@ public class KeyCache {
 		cache = new HashMap<Class<?>, Key<?>>();
 	}
 
+	/**
+	 * Create Guice Key object for given class and {@link EmptyObject} annotation. If such key has been
+	 * already created, return cached value.
+	 * 
+	 * @param clazz Class to be wrapped by Key object
+	 * @return Key object created using {@code clazz} parameter
+	 */
 	@SuppressWarnings("unchecked")
 	public <T> Key<T> getKey(final Class<T> clazz) {
 		Key<T> key = (Key<T>) cache.get(clazz);
