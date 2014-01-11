cc6418aa762f0b7d11f4d981a42ac7b837549fc6
Tomasz Rękawek <trekawek@gmail.com>
2014-01-11 22:02:39 +0100

    fixed typo in the filter name

--- slice-core/src/main/java/com/cognifide/slice/core/internal/filter/ContextRequestFilter.java
+++ slice-core/src/main/java/com/cognifide/slice/core/internal/filter/ContextRequestFilter.java
@@ -1,15 +1,25 @@
 package com.cognifide.slice.core.internal.filter;
 
 /*
- * #%L Slice - Core $Id:$ $HeadURL:$ %% Copyright (C) 2012 Cognifide Limited %% Licensed under the Apache
- * License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You
- * may obtain a copy of the License at
+ * #%L
+ * Slice - Core
+ * $Id:$
+ * $HeadURL:$
+ * %%
+ * Copyright (C) 2012 Cognifide Limited
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  * 
- * http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  * 
- * Unless required by applicable law or agreed to in writing, software distributed under the License is
- * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and limitations under the License. #L%
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
  */
 
 import java.io.IOException;
@@ -52,10 +62,10 @@ import com.cognifide.slice.util.SliceUtil.ConstantContextProvider;
 @Properties({
 		@Property(name = Constants.SERVICE_DESCRIPTION, value = "Filter that is injected into request chain to provide access to request, resource and response."),
 		@Property(name = Constants.SERVICE_VENDOR, value = "Cognifide"),
-		@Property(name = Constants.SERVICE_RANKING, intValue = ContextRequstFilter.RANKING),
+		@Property(name = Constants.SERVICE_RANKING, intValue = ContextRequestFilter.RANKING),
 		@Property(name = "filter.scope", value = {"request","forward"}) })
 // @formatter:on
-public class ContextRequstFilter implements Filter, RequestContextProvider {
+public class ContextRequestFilter implements Filter, RequestContextProvider {
 
 	@Reference
 	private InjectorsRepository injectorsRepo;
