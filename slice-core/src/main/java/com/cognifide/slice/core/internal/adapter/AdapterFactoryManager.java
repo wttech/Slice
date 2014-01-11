abea2d75b10bb93c7e2886e256ee0bf06a39d988
Tomasz Rękawek <trekawek@gmail.com>
2014-01-11 22:47:23 +0100

    created SliceResourceScanner

--- slice-core/src/main/java/com/cognifide/slice/core/internal/adapter/AdapterFactoryManager.java
+++ slice-core/src/main/java/com/cognifide/slice/core/internal/adapter/AdapterFactoryManager.java
@@ -22,11 +22,10 @@
 
 package com.cognifide.slice.core.internal.adapter;
 
-import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Dictionary;
 import java.util.HashMap;
 import java.util.Hashtable;
-import java.util.List;
 import java.util.Map;
 
 import org.apache.felix.scr.annotations.Activate;
@@ -39,15 +38,13 @@ import org.apache.sling.api.resource.Resource;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceRegistration;
 import org.osgi.service.component.ComponentContext;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import com.cognifide.slice.api.context.RequestContextProvider;
 import com.cognifide.slice.api.injector.InjectorConfig;
 import com.cognifide.slice.api.injector.InjectorsRepository;
 import com.cognifide.slice.core.internal.injector.InjectorListener;
+import com.cognifide.slice.core.internal.scanner.SliceResourceScanner;
 import com.google.inject.Injector;
-import com.google.inject.Module;
 
 /**
  * This component is responsible for creation and destruction of the AdapterFactory services. Every time a
@@ -61,77 +58,52 @@ import com.google.inject.Module;
 @Service
 public class AdapterFactoryManager implements InjectorListener {
 
-	private static final Logger LOG = LoggerFactory.getLogger(AdapterFactoryManager.class);
-
 	@Reference
 	private InjectorsRepository repository;
 
 	@Reference
 	private RequestContextProvider requestContextProvider;
 
-	private Map<AdaptableModule, List<String>> modules;
-
-	private Map<AdaptableModule, ServiceRegistration> registrationByModule;
+	private Map<String, ServiceRegistration> registrationByInjector;
 
 	private BundleContext bundleContext;
+
+	private SliceResourceScanner scanner;
 
 	@Activate
 	public void activate(ComponentContext componentContext) {
 		bundleContext = componentContext.getBundleContext();
-		modules = new HashMap<AdaptableModule, List<String>>();
-		registrationByModule = new HashMap<AdaptableModule, ServiceRegistration>();
+		registrationByInjector = new HashMap<String, ServiceRegistration>();
+		scanner = new SliceResourceScanner(bundleContext);
 	}
 
 	@Deactivate
 	public void deactivate() {
-		for (ServiceRegistration adapterFactory : registrationByModule.values()) {
+		for (ServiceRegistration adapterFactory : registrationByInjector.values()) {
 			adapterFactory.unregister();
 		}
 	}
 
 	@Override
 	public void injectorCreated(Injector injector, InjectorConfig config) {
-		for (Module m : config.getModules()) {
-			if (m instanceof AdaptableModule) {
-				registerModule((AdaptableModule) m, config.getName());
-			}
-		}
+		Collection<Class<?>> classes = scanner.findSliceResources(config.getBundleNameFilter(),
+				config.getBasePackage());
+		ServiceRegistration registration = createAdapterFactory(classes, config.getName());
+		registrationByInjector.put(config.getName(), registration);
 	}
 
 	@Override
 	public void injectorDestroyed(Injector injector, InjectorConfig config) {
-		for (Module m : config.getModules()) {
-			if (m instanceof AdaptableModule) {
-				unregisterModule((AdaptableModule) m, config.getName());
-			}
+		ServiceRegistration registration = registrationByInjector.remove(config.getName());
+		if (registration != null) {
+			registration.unregister();
 		}
 	}
 
-	private void registerModule(AdaptableModule module, String name) {
-		if (!modules.containsKey(module)) {
-			modules.put(module, new ArrayList<String>());
-			createAdapterFactory(module, name);
-		}
-		modules.get(module).add(name);
-	}
-
-	private void unregisterModule(AdaptableModule module, String name) {
-		List<String> injectors = modules.get(module);
-		if (injectors == null) {
-			LOG.error("Can't unregister nonexisting module {} for injector {}", new Object[] { module, name });
-			return;
-		}
-		injectors.remove(name);
-		if (injectors.isEmpty()) {
-			destroyAdapterFactory(module);
-			modules.remove(module);
-		}
-	}
-
-	private void createAdapterFactory(AdaptableModule module, String name) {
-		String[] adapterClasses = new String[module.getClasses().size()];
+	private ServiceRegistration createAdapterFactory(Collection<Class<?>> classes, String name) {
+		String[] adapterClasses = new String[classes.size()];
 		int i = 0;
-		for (Class<?> clazz : module.getClasses()) {
+		for (Class<?> clazz : classes) {
 			adapterClasses[i++] = clazz.getName();
 		}
 
@@ -142,13 +114,6 @@ public class AdapterFactoryManager implements InjectorListener {
 		ServiceRegistration registration = bundleContext.registerService(AdapterFactory.class.getName(),
 				factory, properties);
 
-		registrationByModule.put(module, registration);
-	}
-
-	private void destroyAdapterFactory(AdaptableModule module) {
-		ServiceRegistration registration = registrationByModule.remove(module);
-		if (registration != null) {
-			registration.unregister();
-		}
+		return registration;
 	}
 }
