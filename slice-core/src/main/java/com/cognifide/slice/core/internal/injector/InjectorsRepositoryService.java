345fb98d3619fd40d69ff9ef6a6c8d6ba5aeaf27
Maciej Majchrzak <maciej.majchrzak@cognifide.com>
2014-01-16 12:24:19 +0100

    Fixing merge

--- slice-core/src/main/java/com/cognifide/slice/core/internal/injector/InjectorsRepositoryService.java
+++ slice-core/src/main/java/com/cognifide/slice/core/internal/injector/InjectorsRepositoryService.java
@@ -1,26 +1,26 @@
 package com.cognifide.slice.core.internal.injector;
 
 /*-
-* #%L
-* Slice - Core
-* $Id:$
-* $HeadURL:$
-* %%
-* Copyright (C) 2012 Cognifide Limited
-* %%
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-* 
-*      http://www.apache.org/licenses/LICENSE-2.0
-* 
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-* #L%
-*/
+ * #%L
+ * Slice - Core
+ * $Id:$
+ * $HeadURL:$
+ * %%
+ * Copyright (C) 2012 Cognifide Limited
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
 
 import java.util.Collection;
 
@@ -45,10 +45,8 @@ import com.google.inject.Injector;
 @Component(immediate = true)
 @Service
 // @formatter:off
-@Properties({
-	@Property(name = Constants.SERVICE_DESCRIPTION, value = "Repository of all Slice injectors."),
-	@Property(name = Constants.SERVICE_VENDOR, value = "Cognifide")
-})
+@Properties({ @Property(name = Constants.SERVICE_DESCRIPTION, value = "Repository of all Slice injectors."),
+		@Property(name = Constants.SERVICE_VENDOR, value = "Cognifide") })
 // @formatter:on
 public final class InjectorsRepositoryService implements InjectorsRepository {
 
@@ -78,11 +76,4 @@ public final class InjectorsRepositoryService implements InjectorsRepository {
 		return injectors.getInjectorNames();
 	}
 
-	protected void bindInjectors(final InjectorConfig config) {
-		injectors.registerInjector(config);
-	}
-
-	protected void unbindInjectors(final InjectorConfig config) {
-		injectors.unregisterInjector(config);
-	}
 }
