0d25876cf325c4d4558ef7adf3ccbd4fc3aab37d
mariusz_kubis <mariusz,kubis@cognifide.com>
2015-04-15 12:18:15 +0200

    Mapping Strategy test: unused setters

--- slice-test/slice-mapper-test/src/main/java/com/cognifide/slice/mapper/annotation/MappingStrategyAllModel.java
+++ slice-test/slice-mapper-test/src/main/java/com/cognifide/slice/mapper/annotation/MappingStrategyAllModel.java
@@ -14,15 +14,7 @@ public class MappingStrategyAllModel {
 		return field1;
 	}
 
-	public void setField1(String field1) {
-		this.field1 = field1;
-	}
-
 	public String getField2() {
 		return field2;
-	}
-
-	public void setField2(String field2) {
-		this.field2 = field2;
 	}
 }
