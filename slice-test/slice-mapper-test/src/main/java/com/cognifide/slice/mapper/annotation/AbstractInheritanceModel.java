6170aec042bdc5430dd9dc840d46487c93389b7a
mariusz_kubis <mariusz.kubis.aha@gmail.com>
2015-04-21 15:30:30 +0200

    Mapping strategy in Inheritances models: tests

--- slice-test/slice-mapper-test/src/main/java/com/cognifide/slice/mapper/annotation/AbstractInheritanceModel.java
+++ slice-test/slice-mapper-test/src/main/java/com/cognifide/slice/mapper/annotation/AbstractInheritanceModel.java
@@ -3,9 +3,29 @@ package com.cognifide.slice.mapper.annotation;
 /**
  * @author Mariusz Kubiś Date: 15.04.15
  */
-public class AbstractInheritanceModel {
+@SliceResource(MappingStrategy.ANNOTATED)
+class AbstractInheritanceModelAnnotated {
 
 	@JcrProperty
+	private String field1;
+
+	public String getField1() {
+		return field1;
+	}
+}
+
+@SliceResource(MappingStrategy.ALL)
+class AbstractInheritanceModelAll {
+
+	private String field1;
+
+	public String getField1() {
+		return field1;
+	}
+}
+
+class AbstractInheritanceModelWithout {
+
 	private String field1;
 
 	public String getField1() {
--- slice-test/slice-mapper-test/src/main/java/com/cognifide/slice/mapper/annotation/InheritanceModel.java
+++ slice-test/slice-mapper-test/src/main/java/com/cognifide/slice/mapper/annotation/InheritanceModel.java
@@ -3,10 +3,80 @@ package com.cognifide.slice.mapper.annotation;
 /**
  * @author Mariusz Kubiś Date: 15.04.15
  */
-@SliceResource
-public class InheritanceModel extends AbstractInheritanceModel {
+@SliceResource(MappingStrategy.ANNOTATED)
+class InheritanceModelAnnotated extends AbstractInheritanceModelAnnotated {
 
 	@JcrProperty
+	private String field2;
+
+	public String getField2() {
+		return field2;
+	}
+}
+
+@SliceResource(MappingStrategy.ANNOTATED)
+class InheritanceModelAnnotatedAll extends AbstractInheritanceModelAll {
+
+	@JcrProperty
+	private String field2;
+
+	public String getField2() {
+		return field2;
+	}
+}
+
+@SliceResource(MappingStrategy.ANNOTATED)
+class InheritanceModelAnnotatedWithout extends AbstractInheritanceModelWithout {
+
+	@JcrProperty
+	private String field2;
+
+	public String getField2() {
+		return field2;
+	}
+}
+
+@SliceResource(MappingStrategy.ALL)
+class InheritanceModelAll extends AbstractInheritanceModelAll {
+
+	private String field2;
+
+	public String getField2() {
+		return field2;
+	}
+}
+
+@SliceResource(MappingStrategy.ALL)
+class InheritanceModelAllAnnotated extends AbstractInheritanceModelAnnotated {
+
+	private String field2;
+
+	public String getField2() {
+		return field2;
+	}
+}
+
+@SliceResource(MappingStrategy.ALL)
+class InheritanceModelAllWithout extends AbstractInheritanceModelWithout {
+
+	private String field2;
+
+	public String getField2() {
+		return field2;
+	}
+}
+
+class InheritanceModelWithoutAll extends AbstractInheritanceModelAll {
+
+	private String field2;
+
+	public String getField2() {
+		return field2;
+	}
+}
+
+class InheritanceModelWithoutAnnotated extends AbstractInheritanceModelAnnotated {
+
 	private String field2;
 
 	public String getField2() {
