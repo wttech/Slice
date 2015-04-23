14932c58fabf1349fd0f8222d7d6a81f2e267334
krzysztof.ryk@cognifide.com <krzysztof.ryk@cognifide.com>
2015-04-23 15:04:25 +0200

    SliceReference test refactoring.

--- slice-test/slice-mapper-test/src/test/groovy/com/cognifide/slice/mapper/annotation/SliceReferenceTest.groovy
+++ slice-test/slice-mapper-test/src/test/groovy/com/cognifide/slice/mapper/annotation/SliceReferenceTest.groovy
@@ -8,29 +8,50 @@ import org.junit.Assert
  */
 class SliceReferenceTest extends BaseSetup {
 
-    def "Slice Reference test"() {
-        setup:
+    def setupSpec() {
+        setup: "Create test content, under '/content/foo, with two properties: 'text' and 'style'"
         pageBuilder.content {
             foo("cq:PageContent") {
                 "jcr:content"("text": "Test1", "style": "Style", "size": 5)
             }
         }
-
+        and: "Create a test content, under '/content/bar', with property 'text'"
         pageBuilder.content {
             bar("cq:PageContent") {
                 "jcr:content"("text": "Test")
             }
         }
+    }
 
-        expect:
+    def "Created content exists"() {
+        expect: "Created content exists"
         assertPageExists("/content/bar")
         assertPageExists("/content/foo")
-        SliceReferenceModel sliceReferenceModel = modelProvider.get(SliceReferenceModel.class, "/content/bar/jcr:content")
-        Assert.assertNotNull(sliceReferenceModel)
-        Assert.assertEquals(sliceReferenceModel.getText(), "Test")
+    }
+
+    def "Slice Reference test"() {
+        def path = "/content/bar";
+        setup: "Get a model"
+        SliceReferenceModel sliceReferenceModel = modelProvider.get(SliceReferenceModel.class, path + "/jcr:content")
+        and: "Get a property model from Slice Reference Model"
         JcrPropertyModel propertyModel = sliceReferenceModel.getPropertyModel()
+
+        expect: "Model is not null"
+        Assert.assertNotNull(sliceReferenceModel)
+
+        and: "Referenced Model is not null"
+        Assert.assertNotNull(propertyModel)
+
+        and: "Model has a correct value for property 'text'"
+        Assert.assertEquals(sliceReferenceModel.getText(), "Test")
+
+        and: "Referenced Model has a correct value for a property 'text'"
         Assert.assertEquals(propertyModel.getText(), "Test1")
+
+        and: "Referenced Model has a correct value for a property 'secondProperty'"
         Assert.assertEquals(propertyModel.getSecondProperty(), "Style")
+
+        and: "Referenced Model has a correct value for a property 'size'"
         Assert.assertEquals(propertyModel.getSize(), 5)
     }
 }
