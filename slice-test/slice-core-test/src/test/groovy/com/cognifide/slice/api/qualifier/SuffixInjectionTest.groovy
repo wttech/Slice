4c19b675ade3526791ad5c84a055a2ab256d397d
krzysztof.ryk@cognifide.com <krzysztof.ryk@cognifide.com>
2015-04-23 15:14:11 +0200

    Removed redundant models, updated tests.

--- slice-test/slice-core-test/src/test/groovy/com/cognifide/slice/api/qualifier/SuffixInjectionTest.groovy
+++ slice-test/slice-core-test/src/test/groovy/com/cognifide/slice/api/qualifier/SuffixInjectionTest.groovy
@@ -1,30 +1,30 @@
 package com.cognifide.slice.api.qualifier
 
+import com.google.inject.Key
 import junit.framework.Assert
 
 /**
  * Created by Jaromir Celejewski on 2015-04-22.
  */
-class SuffixInjectionTest extends InjectionTestBase{
+class SuffixInjectionTest extends InjectionTestBase {
 
     def "Get Suffix"() {
-        setup: "Init model"
+        setup: "Initialize context with a filled request"
         richRequestContext()
-        def SuffixInjectionModel model = modelProvider.get(SuffixInjectionModel.class, '/content/foo')
-        def suffix = model.getSuffix()
+        and: "Get a suffix from injector"
+        def suffix = injector.getInstance(Key.get(String.class, Suffix.class))
 
-        expect: "There is a suffix equal to "
+        expect: "Suffix is equal to 'suff' "
         Assert.assertEquals("suff", suffix)
     }
 
-    def "Get Empty Suffix"() {
-        setup: "Init model"
+    def "Get Suffix - empty request"() {
+        setup: "Initialize context with an empty request"
         emptyRequestContext()
-        def SuffixInjectionModel model = modelProvider.get(SuffixInjectionModel.class, '/content/foo')
-        def suffix = model.getSuffix()
+        and: "Get a suffix from injector"
+        def suffix = injector.getInstance(Key.get(String.class, Suffix.class))
 
-        expect: "Suffix is an empty string"
-        Assert.assertEquals(suffix, "")
+        expect: "Suffix is equal to 'suff' "
+        Assert.assertEquals("", suffix)
     }
-
 }
