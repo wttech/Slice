6cf922731a9c2a236472b5cb798f271e3228a6db
Jaromir Celejewski <jaromir.celejewski@cognifide.com>
2015-04-24 13:47:35 +0200

    adapters test

---
+++ slice-test/slice-core-test/src/test/groovy/com/cognifide/slice/core/internal/adapter/SliceAdapterFactoryTest.groovy
@@ -0,0 +1,33 @@
+package com.cognifide.slice.core.internal.adapter
+
+import com.cognifide.slice.test.module.SimpleModel
+import junit.framework.Assert
+
+
+/**
+ * Created by Jaromir Celejewski on 2015-04-23.
+ */
+class SliceAdapterFactoryTest extends AdapterBaseSetup {
+
+    def setupSpec() {
+        setup: "Create simple, initial content - node 'foo' with property 'prop1' set to 'prop1Value'"
+        pageBuilder.content {
+            foo("foo") {
+                "jcr:content"("prop1": "prop1Value")
+            }
+        }
+    }
+
+    def "resource is adaptable to multiple types"() {
+        setup:
+        def path = "/content/foo/jcr:content"
+        def resource = resourceResolver.getResource(path)
+        def SimpleModel model = resource.adaptTo(SimpleModel.class)
+
+        expect:
+        Assert.assertNotNull(model)
+
+        and:
+        Assert.assertEquals("prop1Value", model.getProp1())
+    }
+}
