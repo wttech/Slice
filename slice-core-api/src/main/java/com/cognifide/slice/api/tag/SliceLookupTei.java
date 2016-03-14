a7778ac710dbc32b3be87021846b5d4e315718e1
Jan Kuźniak <jan.kuzniak@cognifide.com>
2016-03-14 16:06:52 +0100

    SLICE-53: removed duplicate attribute (cls), code cleanuip

--- slice-core-api/src/main/java/com/cognifide/slice/api/tag/SliceLookupTei.java
+++ slice-core-api/src/main/java/com/cognifide/slice/api/tag/SliceLookupTei.java
@@ -1,19 +1,44 @@
+/*-
+ * #%L
+ * Slice - Core API
+ * %%
+ * Copyright (C) 2012 Cognifide Limited
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
 package com.cognifide.slice.api.tag;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.servlet.jsp.tagext.TagData;
 import javax.servlet.jsp.tagext.TagExtraInfo;
 import javax.servlet.jsp.tagext.VariableInfo;
 
-/**
- * User: krzysztof.ryk@solsoft.pl Date: 11/7/13 1:07 PM
- */
 public class SliceLookupTei extends TagExtraInfo {
 
-	private static final String ATTRIBUTE_CLS = "cls";
+	private static final int VARIABLE_SCOPE = VariableInfo.AT_BEGIN;
+
+	private static final boolean DECLARE_NEW_VARIABLE = true;
+
+	private static final String ATTRIBUTE_TYPE = "type";
 
 	private static final String ATTRIBUTE_VAR = "var";
 
 	private static final String DEFAULT_TYPE = "java.lang.Object";
+
+	private static final Pattern typePattern = Pattern.compile("<%=\\s*(.*).class\\s*%>");
 
 	/**
 	 * Get variable info for controller tag. This method adds additional info to object placed in the page
@@ -25,12 +50,18 @@ public class SliceLookupTei extends TagExtraInfo {
 	 */
 	@Override
 	public VariableInfo[] getVariableInfo(TagData data) {
-		String cls = data.getAttributeString(ATTRIBUTE_CLS);
-		if (cls == null) {
-			cls = DEFAULT_TYPE;
+		String type = data.getAttributeString(ATTRIBUTE_TYPE);
+		Matcher matcher = typePattern.matcher(type);
+
+		String className = DEFAULT_TYPE;
+		if (matcher.matches()) {
+			className = matcher.group(1);
 		}
-		return new VariableInfo[] { new VariableInfo(data.getAttributeString(ATTRIBUTE_VAR), cls, true,
-				VariableInfo.AT_BEGIN) };
+
+		String variableName = data.getAttributeString(ATTRIBUTE_VAR);
+
+		return new VariableInfo[] {
+				new VariableInfo(variableName, className, DECLARE_NEW_VARIABLE, VARIABLE_SCOPE) };
 	}
 
 }
